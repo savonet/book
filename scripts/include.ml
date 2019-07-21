3ee4e526b8b8ff2824fa9d4dad97c2cf373ca014
Samuel Mimram <smimram@gmail.com>
2019-07-21 19:33:03 +0200

    Recursive include.

--- scripts/include.ml
+++ scripts/include.ml
@@ -4,11 +4,11 @@ open Pandoc
 
 let () =
   let json = Yojson.Basic.from_channel stdin in
-  let f b =
+  let rec f b =
     if is_code_block b && List.mem "include" (code_block_classes b) then
       let contents = code_block_contents b in
       let j = json_of_md_file contents in
-      blocks j
+      List.flatten (List.map f (blocks j))
     else
       [b]
   in
