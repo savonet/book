1f60732471a776709fa65cd5d46bd3ad906f25c5
Samuel Mimram <smimram@gmail.com>
2021-04-28 10:10:18 +0200

    pandoc is now externalized.

--- scripts/inspect.ml
+++
@@ -1,7 +0,0 @@
-(** Simple pandoc extension to print the JSON. *)
-
-let () =
-  let json = Yojson.Basic.from_channel stdin in
-  let s = Yojson.Basic.pretty_to_string json in
-  Printf.eprintf "%s\n%!" s;
-  Printf.printf "%s\n%!" s
